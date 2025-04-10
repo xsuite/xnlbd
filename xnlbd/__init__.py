3dd782eb0cc5addebb6aa1aa312a42280d0d35e5
Dora Veres <veresdoraerzsebet@gmail.com>
2025-04-10 13:21:52 +0200

    Flatter access to functions and classes

--- xnlbd/__init__.py
+++ xnlbd/__init__.py
@@ -1,3 +1,43 @@
 from ._version import __version__
 
-from . import analyse, tools, track, visualise
+# from . import analyse, tools, track, visualise
+from .analyse.chaos import (
+    COORD_LIST,
+    NORM_COORD_LIST,
+    GhostParticleManager,
+    gali_extractor,
+    reverse_error_method,
+    track_displacement,
+    track_displacement_birkhoff,
+)
+from .analyse.normal_forms import (
+    Map,
+    NormalForm4D,
+    PolyCavity4D,
+    PolyDipoleEdge4D,
+    PolyDrift4D,
+    PolyHenonmap4D,
+    PolyIdentity4D,
+    PolyLine4D,
+    PolyMarker4D,
+    PolyMultipole4D,
+    Polynom,
+    PolyOctupole4D,
+    PolyReferenceEnergyIncrease4D,
+    PolySextupole4D,
+    PolySimpleThinBend4D,
+    PolySimpleThinQuadrupole4D,
+    PolySRotation4D,
+    PolyXYShift4D,
+    PolyZetaShift4D,
+    Term,
+)
+from .analyse.rdt import calculate_rdts
+from .tools import H5pyWriter, LocalWriter, NormedParticles, birkhoff_weights
+from .track import Henonmap, ModulatedHenonmap, RandomNormalKick
+from .visualise.fixed_points import FPFinder
+from .visualise.orbits import get_orbit_points
+from .visualise.separatrix import (
+    approximate_separatrix_by_region_2D,
+    separatrix_points_2D,
+)
