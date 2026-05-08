606ed42ddc153576b2c8cf9b6b67adaa09c1710d
Dora Veres <veresdoraerzsebet@gmail.com>
2026-05-08 14:33:01 +0200

    Small fix in docstring

--- xnlbd/analyse/normal_forms/numeric_normal_forms.py
+++ xnlbd/analyse/normal_forms/numeric_normal_forms.py
@@ -41,8 +41,8 @@ class NormalForm4D:
             - res_case: integer, 0 for nonresonant normal forms, 1 for exactly
               resonant normal forms, 2 for quasiresonant normal forms
             - res_eig: list of complex resonant eigenvalues,
-              i.e. [$e^{2i\pi Q_{x,res}}$, $e^{-2i\pi Q_{x,res}}$,
-              $e^{2i\pi Q_{y,res}}$, $e^{-2i\pi Q_{y,res}}$], only needed if
+              i.e. [$e^{2i\\pi Q_{x,res}}$, $e^{-2i\\pi Q_{x,res}}$,
+              $e^{2i\\pi Q_{y,res}}$, $e^{-2i\\pi Q_{y,res}}$], only needed if
               res_case is 1 or 2
             - res_basis1: list or integers, [n, m] which satisfy the resonance
               condition n*Q_x+m*Q_y=p, only needed if res_space_dim is 1 or 2
